dd0653fdcdfe56f41a560eef817537cd0ee00ad1
saddique <saddique_uet@yahoo.com>
2019-09-04 00:18:18 +0400

    first commit

---
+++ Deploy-StorageAccount.ps1
@@ -0,0 +1,29 @@
+﻿{
+$resourceGroupName = 'SantiaDemo'
+$resourceGroupLocation = 'Southeast Asia'
+$resoruceDeploymentName= "santiastorageaccount"
+$templatePath = $env:SystemDrive + '\' + 'santia\StorageAccount\StorageAccount'
+$templateName= 'azuredeploy.json'
+$template = $templatePath + '\' + $templateName
+}
+
+
+# Creating a new Resource Group
+
+{
+New-AzureRmResourceGroup `
+  -Location $resourceGroupLocation `
+  -Name $resourceGroupName `
+  -verbose -Force
+}
+
+# Creation of Account Storage
+
+{
+ New-AzureRmResourceGroupDeployment `
+   -Name $resoruceDeploymentName `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile $template `
+  -verbose -Force
+
+}
